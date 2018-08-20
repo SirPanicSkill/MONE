05d498a3b7dbec973aa2c3c60760769202cc8b7c
SirPanicSkill <txwnik@live.ru>
2018-08-20 15:46:23 +0300

    add main.js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/* JqvaScript code */
+document.addEventListener('DOMContentLoaded', function() {
+	
+});
+
+/*globals jQuery, document */
+$(function() {
+	"use strict";
+	
+});
+
+/* после загрузки страницы */
+$(window).on('load', function(){
+	"use strict";
+});
