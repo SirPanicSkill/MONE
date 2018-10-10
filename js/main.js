0b1cd937589b538aa54dc9f942552515f3769b10
SirPanicSkill <txwnik@live.ru>
2018-10-10 16:03:52 +0300

    fix mobile

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', function() {
 /*globals jQuery, document */
 $(function() {
 	"use strict";
-
 });
 
 /* после загрузки страницы */
@@ -16,7 +15,7 @@ $(window).on('load', function(){
 		sectionContainer: "article",     // sectionContainer accepts any kind of selector in case you don't want to use section
 		easing: "ease-in-out",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
 										 // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-		animationTime: 800,             // AnimationTime let you define how long each section takes to animate
+		animationTime: 500,             // AnimationTime let you define how long each section takes to animate
 		pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
 		updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
 		beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
